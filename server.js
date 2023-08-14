63c2c95021d0eb1de25e2f494cebc55527437ee8
David Williams <davwil00@gmail.com>
2023-08-14 21:14:29 +0100

    adjustments to run on pi

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.all(
       })
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8300;
 app.listen(port, async () => {
   console.log(`Express server listening on port ${port}`);
 
