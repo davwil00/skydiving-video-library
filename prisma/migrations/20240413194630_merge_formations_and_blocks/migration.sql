c87ac9d6be4dc23194caaea3a7f6956dc341ade1
David Williams <14893549+davwil00@users.noreply.github.com>
2024-04-17 17:11:18 +0100

    Fix migration

--- prisma/migrations/20240413194630_merge_formations_and_blocks/migration.sql
+++ prisma/migrations/20240413194630_merge_formations_and_blocks/migration.sql
@@ -3,22 +3,21 @@ CREATE TABLE "new_FlightFormation" (
    "formationId" TEXT NOT NULL,
    "order" INTEGER NOT NULL,
 
-   PRIMARY KEY ("flightId", "formationId"),
+   PRIMARY KEY ("flightId", "formationId", "order"),
    CONSTRAINT "FlightFormation_flightId_fkey" FOREIGN KEY ("flightId") REFERENCES "Flight" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 INSERT INTO "new_FlightFormation" ("flightId", "formationId", "order")
-    SELECT "flightId", "formationLetter", "order" from "FlightFormation";
+    SELECT "flightId", "formationLetter", "order" FROM "FlightFormation";
 INSERT INTO "new_FlightFormation" ("flightId", "formationId", "order")
-    SELECT
-        "B",
-        "A",
-        (SELECT max("order")
-         FROM "FlightFormation"
-         WHERE flightId = ff.flightId) + 1 as "order"
-    FROM "_BlocksToFlight" btf
-    JOIN "FlightFormation" ff
-      ON btf.B = ff.flightId;
+SELECT "B",
+       "A",
+       "order"
+FROM "_BlocksToFlight" btf
+         JOIN (SELECT "flightId", max("order") + 1 AS "order"
+               FROM "FlightFormation" ff
+               GROUP BY ff."flightId") AS ff
+              ON btf."B" = ff."flightId";
 
 DROP TABLE "_BlocksToFlight";
 DROP TABLE "FlightFormation";
