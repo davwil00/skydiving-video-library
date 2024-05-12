633b3510cca0dfb202a3f71a3d8d05d751f1a696
David Williams <14893549+davwil00@users.noreply.github.com>
2024-05-12 17:28:55 +0100

    Update script to copy both sources

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,7 @@
-cd ~/Movies/skydiving/2024-01-23/
-sftp david@pi:/home/david/projects/skydiving-video-library/public/video-data/pending <<EOF
+cd ~/Movies/skydiving/2024-05-08/
+sftp david@pi4:/home/david/projects/skydiving-video-library/public/video-data/pending <<EOF
 put source02*.mp4
+lcd compressed
+put source01*.mp4
 exit
 EOF
