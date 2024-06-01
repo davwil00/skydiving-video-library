59c0b16ba3a47915237f8cdaa6b930f4c41012cc
David Williams <14893549+davwil00@users.noreply.github.com>
2024-06-01 15:12:08 +0100

    Add ordering for formations in sql

--- app/models/flights.server.ts
+++ app/models/flights.server.ts
@@ -96,7 +96,7 @@ export function getFlight(flightId: string) {
         include: {
             session: true,
             flyers: true,
-            formations: true
+            formations: { orderBy: { order: 'asc' } }
         }
     });
 }
