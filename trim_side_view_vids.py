2c60aaba4c0c24ccdc87d9e63edd94497213a9b7
David Williams <14893549+davwil00@users.noreply.github.com>
2025-05-09 18:10:00 +0100

    Tweaks to processing script

--- trim_side_view_vids.py
+++ trim_side_view_vids.py
@@ -4,7 +4,6 @@ import sys
 import ffmpeg
 import pathlib
 
-
 def get_duration(file):
     try:
         probe = ffmpeg.probe(file)
@@ -27,19 +26,15 @@ def remove_audio(file, output):
     return ffmpeg.input(file).output(output, c='copy', an=None)
 
 def process_files():
-    if len(sys.argv) != 2:
-        print('Usage: python trim_side_view_vids.py <folder_name> e.g. 2025-01-01')
-        sys.exit(1)
-    folder_name = sys.argv[1]
-    folder = f'/Users/david.williams/Movies/skydiving/{folder_name}'
-    files = list(pathlib.Path(folder).rglob('source0*.mp4'))
+    files = list(pathlib.Path(folder).rglob('source0*.mp4')) + list(pathlib.Path(folder).rglob('Flight*.mp4'))
     os.makedirs(os.path.join(folder, 'processed'), exist_ok=True)
+    print(f'Processing {len(files)} files')
 
     for file in files:
         print(f'Processing {file}')
         split_path = os.path.split(file)
-        output = os.path.realpath(os.path.join(split_path[0], '../processed', split_path[1]))
-        if split_path[1].startswith('source01'):
+        output = os.path.realpath(os.path.join(split_path[0], 'processed', split_path[1]))
+        if split_path[1].startswith('source01') or split_path[1].startswith('Flight'):
             stream = trim(file, output)
         elif split_path[1].startswith('source02'):
             stream = remove_audio(file, output)
@@ -48,5 +43,20 @@ def process_files():
         print(stream.compile())
         stream.run(overwrite_output=True)
 
+def rename_files():
+    top_cam_videos = sorted([file for file in os.scandir(folder) if file.name.startswith('source02')], key=lambda x: x.name)
+    side_cam_videos = sorted([file for file in os.scandir(folder) if file.name.startswith('Flight')], key=lambda x: x.name)
+    # [print(f'renaming {entry[1].path} to {os.path.join(folder, "source01-" + entry[0].name[9:])}') for entry in zip(top_cam_videos, side_cam_videos)]
+    [os.rename(entry[1].path, os.path.join(folder, 'source01-' + entry[0].name[9:])) for entry in zip(top_cam_videos, side_cam_videos)]
+
 if __name__ == '__main__':
-    process_files()
+    if len(sys.argv) != 2:
+        print('Usage: python trim_side_view_vids.py <path to folder> e.g. /Users/place/2025-01-01')
+        sys.exit(1)
+    folder = sys.argv[1]
+    print('Do you want to rename the files? (y/n)')
+    if input() == 'y':
+        rename_files()
+    print('Do you want to trim the files? (y/n)')
+    if input() == 'y':
+        process_files()
