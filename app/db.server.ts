393cb685d09b07a0dd09b8199666e260fba00983
David Williams <14893549+davwil00@users.noreply.github.com>
2025-05-25 14:26:00 +0100

    disable query logging

--- app/db.server.ts
+++ app/db.server.ts
@@ -5,7 +5,7 @@ import { singleton } from "./singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 const prisma = singleton("prisma", () => new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
+  log: ['info', 'warn', 'error'],
 }));
 prisma.$connect();
 
