95a63b5a63682b56f0b332e2a85f600a7da3ad63
David Williams <davwil00@gmail.com>
2025-09-28 16:31:19 +0100

    Switch FFMpeg for MediaBunny

--- app/utils/tagUtils.ts
+++ app/utils/tagUtils.ts
@@ -1,68 +1,93 @@
-// @ts-expect-error no types exist for this package
-import ffmetadata from "ffmetadata";
-import type {Dirent} from "fs";
+import {type Dirent, WriteStream, createWriteStream} from "fs";
+import { unlink, rename } from "fs/promises";
+import {Conversion, FilePathSource, Input, MP4, Mp4OutputFormat, Output, StreamTarget} from "mediabunny";
 
-export function readTag(path: string): Promise<TagData> {
-  return new Promise((resolve, reject) => {
-    ffmetadata.read(path, (err: unknown, data: TagData) => {
-      if (err) {
-        console.error("Error reading tags", err)
-        return reject(err)
-      }
-      return resolve(data)
+export async function readTag(path: string): Promise<TagData> {
+    const input = new Input({
+        formats: [MP4],
+        source: new FilePathSource(path),
     });
-  })
+
+    const metadata = await input.getMetadataTags()
+    return {
+        title: metadata.title,
+        artist: metadata.artist,
+        date: metadata.date?.toDateString(),
+        comment: metadata.comment,
+    }
 }
 
-export function writeTag(path: string, tags: TagData): Promise<void> {
-  return new Promise((resolve, reject) => {
-    ffmetadata.write(path, tags, (err: unknown) => {
-      if (err) {
-        console.error("Error writing tag", err)
-        return reject(err)
-      }
-      return resolve()
+export async function writeTag(path: string, tags: TagData): Promise<void> {
+    const tempPath = path + '.tmp'
+    const input = new Input({
+        formats: [MP4],
+        source: new FilePathSource(path),
     })
-  })
+
+    const stream = WriteStream.toWeb(createWriteStream(tempPath))
+    const output = new Output({
+        format: new Mp4OutputFormat(),
+        target: new StreamTarget(stream, { chunked: true, chunkSize: 2**20}),
+    })
+    try {
+        const conversion = await Conversion.init({
+            input,
+            output,
+            tags: (inputTags) => ({
+                ...inputTags,
+                title: tags.title,
+                artist: tags.artist,
+                date: new Date(tags.date!),
+                comment: tags.comment,
+            })
+        })
+        await conversion.execute()
+        await unlink(path)
+        await rename(tempPath, path)
+    } catch (err) {
+        console.error("Unable to write tags", err)
+    } finally {
+        await unlink(tempPath).catch(() => { /* ignore */ })
+    }
 }
 
 export type VideoData = {
-  formationIds: string[];
-  flyers: string[];
-  date: Date | null;
-  view: string;
+    formationIds: string[];
+    flyers: string[];
+    date?: Date | null;
+    view: string;
 };
 
 export async function processFile(file: Dirent, path: string): Promise<VideoData | undefined> {
-  if (file.isFile() && (file.name.endsWith(".mp4") || file.name.endsWith(".av1"))) {
-    console.log("processing file", file.name)
-    const tags = await readTag(`${path}/${file.name}`)
-    const delimiter = tags.title?.includes(',') ? ',' : ''
-    const title: string[] = tags.title?.split(delimiter) || [];
-    return {
-      formationIds: title,
-      flyers: tags.artist?.split("/") || [],
-      date: tags.date ? new Date(tags.date) : null,
-      view: tags.comment!
-    };
-  }
+    if (file.isFile() && (file.name.endsWith(".mp4") || file.name.endsWith(".av1"))) {
+        console.log("processing file", file.name)
+        const tags = await readTag(`${path}/${file.name}`)
+        const delimiter = tags.title?.includes(',') ? ',' : ''
+        const title: string[] = tags.title?.split(delimiter) || [];
+        return {
+            formationIds: title,
+            flyers: tags.artist?.split("/") || [],
+            date: tags.date ? new Date(tags.date) : null,
+            view: tags.comment!
+        };
+    }
 }
 
 export type TagData = {
-  title?: string
-  artist?: string
-  date?: string
-  comment?: string
+    title?: string
+    artist?: string
+    date?: string
+    comment?: string
 }
 
 export function extractIdFromFileName(fileName: string) {
-  if (fileName.startsWith('source')) {
-    return fileName.substring(9)
-  }
+    if (fileName.startsWith('source')) {
+        return fileName.substring(9)
+    }
 
-  return fileName
+    return fileName
 }
 
 export function determineViewFromFilename(fileName: string) {
-  return fileName.startsWith('source01') ? 'SIDE' : 'TOP';
+    return fileName.startsWith('source01') ? 'SIDE' : 'TOP';
 }
