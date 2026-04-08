f359edb14bb25479f72b465284ae3b5b8bd80777
David Williams <14893549+davwil00@users.noreply.github.com>
2026-04-08 21:11:54 +0100

    add allowed action origins
    Fixes issue where posts are rejected with origin/host mismatch

---
+++ react-router.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "@react-router/dev/config";
+
+export default {
+    allowedActionOrigins: [
+        "skydiving.davwil00.co.uk",
+        "skydiving-local.davwil00.co.uk",
+    ],
+} satisfies Config;
