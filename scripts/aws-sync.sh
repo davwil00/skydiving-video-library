ac15f9f572a17c8b8f3e2269ce3e6ea2dd92822f
David Williams <davwil00@gmail.com>
2026-05-12 18:22:42 +0100

    add tv sync

--- scripts/aws-sync.sh
+++ scripts/aws-sync.sh
@@ -25,14 +25,29 @@ sync_solo() {
   fi
 }
 
+sync_tunnel_vision() {
+  local run=${1}
+  src="./public/video-data/tv-library"
+  dest="s3://tunnel-vision-356757819443-eu-west-1-an"
+  if [ "${run}" == "true" ]; then
+    echo "syncing tunnel vision library $src to $dest"
+    aws s3 sync "$src" "$dest"
+  else
+    echo "dryrun tunnel vision library $src to $dest"
+    aws s3 sync --dryrun "$src" "$dest"
+  fi
+}
+
 select_library() {
   local runType=${1}
-  select library in "main" "solo"; do
+  select library in "main" "solo" "tunnel_vision"; do
     case $library in
       main)
         sync_main "$1";;
       solo)
         sync_solo "$1";;
+      tunnel_vision)
+        sync_tunnel_vision "$1";;
     esac
   done
 }
